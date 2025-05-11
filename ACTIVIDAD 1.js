e05c331dd96292627624b47dd0ef12814cb601c1
Ivan Andres Gonzalez <iivan.andres07@gmail.com>
2025-05-11 11:27:45 -0500

    mi commit

--- ACTIVIDAD 1.js
+++ ACTIVIDAD 1.js
@@ -7,7 +7,7 @@ if ((promedio >= 8 && materias >= 7) && (disciplina)){
         console.log("¡Felicidades!, gana la beca")
         }
     
-else if ((promedio >= 5 && promedio < 8) && (materias >= 4 && materias <= 8) && (disciplina)){
+else if ((promedio >= 5 && promedio <= 8) && (materias >= 4 && materias <= 6) && (disciplina)){
         console.log("¡Felicidades!, gana media beca")
          }
 else
